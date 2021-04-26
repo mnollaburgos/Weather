d74dd691d6c14d8ce8b2749bcc02fe6c85122187
Miguel Nolla <nolla.carlos@gmail.com>
2021-04-26 10:29:29 -0400

    update forecast

--- Assets/script.js
+++ Assets/script.js
@@ -70,12 +70,16 @@ function updateWeather(currentWeather){
 //API call for Five-Day Forecast
 function fiveDay(city){
   $.ajax({
-    url: "http://api.openweathermap.org/data/2.5/forecast?q="+city+"&cnt=5&appid=4990a0ba671639637ded74843defb334",
+    url: "http://api.openweathermap.org/data/2.5/forecast?q="+city+"&appid=4990a0ba671639637ded74843defb334",
     method: "GET"
   }).then(function(response) {
     console.log(response);
-
-    updateForecast(response);
+    for(var i=0;i<response.list.length;i++){
+      var time = response.list[i].dt_txt.split(" ");
+      if(time[1] === "15:00:00"){
+        console.log(response.list[i]);
+      }
+    }
   });
 
 }
